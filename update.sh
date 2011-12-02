537d24283b70dfc8a0581c12442bc024e2babc6f
Carlos Cabrera <point_to_null@esdebian.org>
2011-12-02 00:30:55 -0300

    subida nueva versión con css externo

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ROOT="$HOME/documentos/carlos/apuntes/Stanford"
-zim --export --output=./ --format=html --template=Default_with_index ~/documentos/carlos/apuntes/Stanford
+zim --export --output=./ --format=html --template=./template.html ~/documentos/carlos/apuntes/Stanford
