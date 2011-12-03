d2cf3873117e6fa965e2451f2906c687da7a4791
Carlos Cabrera <point_to_null@esdebian.org>
2011-12-03 16:32:45 -0300

    actualizacón automatica

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ROOT="sources"
 zim --export --output=./ --format=html --template=./template.html "$ROOT"
-cp home.html index.html
-git add sources index home.html index.html
+cp Stanford.html index.html
+git add sources Stanford Stanford.html index.html
 git commit -am "actualizacón automatica"
 git push
