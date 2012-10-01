70b107927eb160c3eca4b8b790e51a9f004bf440
Carlos Cabrera <point_to_null@esdebian.org>
2012-10-01 03:11:01 -0300

    cleaned home

---
+++ pelicanconf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'PointToNull'
+SITENAME = "pointtonull's buglist"
+SITEURL = 'http://pointtonull.github.com'
+
+TIMEZONE = 'America/Jujuy'
+
+DEFAULT_LANG = 'es'
+
+# Blogroll
+LINKS =  (
+          ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          )
+
+# Social widget
+SOCIAL = (
+          ('You can add links in your config file', '#'),
+          ('Another social link', '#'),
+         )
+
+DEFAULT_PAGINATION = 10
