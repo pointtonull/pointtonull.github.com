8ef75da4fded032c0e7571db660bb633716bdb64
Carlos M. Cabrera <carlos@whitespaceventures.com>
2019-01-15 16:31:07 -0300

    Generate Pelican site

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ROOT="sources"
-zim --export --output=./ --format=html --template=./template.html "$ROOT"
-git add sources Stanford Stanford.html
-git commit -am "actualizacón automatica"
-git push
